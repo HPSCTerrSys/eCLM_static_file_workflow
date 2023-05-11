552c76f35825f13ce0cbb3dd0f9eae9d01710adc
Christian Poppe <c.poppe@fz-juelich.de>
2023-05-11 14:52:41 +0200

    no landmask applies for me

--- mod_domain.sh
+++ mod_domain.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-surface_file=surface.nc
-domain_file=domain.nc
-landmask_file=landmask.nc
+surface_file=/p/scratch/cjibg31/jibg3105/CESMDataRoot/InputData/lnd/clm2/surfdata_map/surfdata_EUR-0275_hist_78pfts_CMIP6_simyr2005_c230307.nc
+domain_file=/p/scratch/cjibg31/jibg3105/CESMDataRoot/InputData/share/domains/EUR-0275/domain/domain.lnd.EUR-0275.230303.nc
+#landmask_file=landmask.nc
 ncap2 -O -s 'where(LONGXY<0) LONGXY=LONGXY+360' ${surface_file} temp.nc
 mv temp.nc ${surface_file}
 
 ncap2 -O -s 'where(xc<0) xc=xc+360' ${domain_file} temp.nc
 ncap2 -O -s 'where(xv<0) xv=xv+360' temp.nc ${domain_file}
 
-ncrenam -v LANDMASK,mask -v FRAC,frac landmask_file
+#ncrenam -v LANDMASK,mask -v FRAC,frac landmask_file
 
-ncks -A -v mask ${landmask_file} ${domain_file}
+#ncks -A -v mask ${landmask_file} ${domain_file}
 
-ncks -A -v frac ${landmask_file} ${domain_file}
+#ncks -A -v frac ${landmask_file} ${domain_file}
 
 
 
