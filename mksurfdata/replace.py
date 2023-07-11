3d1c9dcabfc6ef8cb75e6fffd248924f1d22c602
Carl Hartick <c.hartick@fz-juelich.de>
2023-07-11 16:04:20 +0200

    Update replacement

--- mksurfdata/replace.py
+++ mksurfdata/replace.py
@@ -9,26 +9,93 @@ path_clm_5 = sys.argv[2]
 clm_3_5_source = xr.open_dataset(path_clm_3_5)
 
 pft_3_5 = clm_3_5_source['PCT_PFT']
+#sai_3_5 = clm_3_5_source['MONTHLY_SAI']
+lai_3_5 = clm_3_5_source['MONTHLY_LAI']
+#top_3_5 = clm_3_5_source['MONTHLY_HEIGHT_TOP']
+#bot_3_5 = clm_3_5_source['MONTHLY_HEIGHT_BOT']
+
+
 pft_3_5 = pft_3_5.to_numpy()
+#sai_3_5 = sai_3_5.to_numpy()
+lai_3_5 = lai_3_5.to_numpy()
+#top_3_5 = top_3_5.to_numpy()
+#bot_3_5 = bot_3_5.to_numpy()
+
 
 
 pft_natural = pft_3_5[0:15,:,:]
-cft = pft_3_5[15:,:,:]
+cft = pft_3_5[15,:,:]
+lakes_3_5 = pft_3_5[17,:,:]
+glacier_3_5 = pft_3_5[18,:,:]
+urban_3_5 = pft_3_5[19,:,:]
+
+
+#sai = sai_3_5[:,0:15,:,:]
+lai = lai_3_5[:,0:15,:,:]
+#top = top_3_5[:,0:15,:,:]
+#bot = bot_3_5[:,0:15,:,:]
+
 
 
 clm5_dest = xr.open_dataset(path_clm_5, mode='a')
 #clm5_dest = clm5_dest.isel(cft=slice(0, 2))
 #clm5_dest = clm5_dest.isel(lsmpft=slice(0, 17))
 
-clm5_dest.attrs['Urban_raw_data_file_name'] = 'None'
+clm5_dest.attrs['Urban_raw_data_file_name'] = 'GLC2000'
 clm5_dest.attrs['Vegetation_type_raw_data_filename'] = 'GLC2000'
 
-clm5_dest['PCT_URBAN'][:] = 0 
+clm5_dest['PCT_URBAN'][2,:,:] = urban_3_5
+clm5_dest['PCT_URBAN'][1,:,:] = 0
+
+clm5_dest['PCT_CROP'][:,:] = cft
+
+clm5_dest['PCT_GLACIER'][:,:] = glacier_3_5
+
+clm5_dest['PCT_LAKE'][:,:] = lakes_3_5
+
+clm5_dest['PCT_NATVEG'][:,:] = np.sum(pft_natural, axis=0)
+
+clm5_dest['PCT_NAT_PFT'][:,:,:] = pft_natural
+
+
+#clm5_dest['MONTHLY_SAI'][:,0:15,:,:]  = sai
+
+clm5_dest['MONTHLY_LAI'][:,0:15,:,:]  = lai
+
+#clm5_dest['MONTHLY_HEIGHT_TOP'][:,0:15,:,:]  = top
+
+#clm5_dest['MONTHLY_HEIGHT_BOT'][:,0:15,:,:]  = bot
 
 
 
-clm5_dest["PCT_NAT_PFT"]=(['natpft', 'lsmlat', 'lsmlon'],  pft_natural)
-clm5_dest["PCT_CFT"]=(['cft', 'lsmlat', 'lsmlon'],  cft)
+#sai_clm5 = clm5_dest['MONTHLY_SAI'].to_numpy()
+
+#lai_clm5 = clm5_dest['MONTHLY_LAI'].to_numpy()
+
+#top_clm5 = clm5_dest['MONTHLY_HEIGHT_TOP'].to_numpy()
+
+#bot_clm5 = clm5_dest['MONTHLY_HEIGHT_BOT'].to_numpy()
+
+
+#sai_clm5[:,0:15,:,:] = sai
+
+#lai_clm5[:,0:15,:,:]  = lai
+
+#top_clm5[:,0:15,:,:]  = top
+
+#bot_clm5[:,0:15,:,:]  = bot
+
+
+
+
+#clm5_dest["PCT_NAT_PFT"]=(['natpft', 'lsmlat', 'lsmlon'],  pft_natural)
+#clm5_dest["MONTHLY_SAI"]=(['time','lsmpft', 'lsmlat', 'lsmlon'],  sai_clm5)
+#clm5_dest["MONTHLY_LAI"]=(['time','lsmpft', 'lsmlat', 'lsmlon'],  lai_clm5)
+#clm5_dest["MONTHLY_HEIGHT_TOP"]=(['time','lsmpft', 'lsmlat', 'lsmlon'],  top_clm5)
+#clm5_dest["MONTHLY_HEIGHT_BOT"]=(['time','lsmpft', 'lsmlat', 'lsmlon'],  bot_clm5)
+
+
+#clm5_dest["PCT_CFT"]=(['cft', 'lsmlat', 'lsmlon'],  cft)
 
 clm5_dest.to_netcdf(path_clm_5)
 
--- mksurfdata/replace_clm5_with_clm3_5_data.sh
+++ mksurfdata/replace_clm5_with_clm3_5_data.sh
@@ -4,14 +4,15 @@
 ##It is still under discussion whether the soillevels are identical but both CLM versions have 10 levles in the surface file. LAI and other plant parameters can only be replaced like this if there are the same number of them in both datasets. 
 
 CLM_3_5_path=/p/scratch/cslts/hartick1/TSMP_EUR-0275_transform_for_eCLM/TSMP_EUR-11/static.resource/10_GatherCLM35/surfdata_CLM_EUR-11_TSMP_FZJ-IBG3_CLMPFLDomain_1592x1544.nc
-CLM5_path=/p/scratch/cslts/hartick1/temp/chris_cut.nc
-CLM5_path_cut_pft=/p/scratch/cslts/hartick1/temp/chris_cut_2.nc
+#CLM5_path=/p/scratch/cslts/hartick1/temp/work_chris_cut.nc
+CLM5_path_cut_pft=/p/scratch/cslts/hartick1/temp/work_chris_cut.nc
 
 
 #ncks -A -v PCT_SAND,PCT_CLAY,MONTHLY_LAI,MONTHLY_SAI,MONTHLY_HEIGHT_TOP,MONTHLY_HEIGHT_BOT $CLM_3_5_path $CLM5_path
-ncks -d cft,0,1 -d lsmpft,0,16 $CLM5_path $CLM5_path_cut_pft
+#ncks -d cft,0,1 -d lsmpft,0,16 $CLM5_path $CLM5_path_cut_pft
 python replace.py $CLM_3_5_path $CLM5_path_cut_pft
 
 
-ncks -A -v PCT_SAND,PCT_CLAY,MONTHLY_LAI,MONTHLY_SAI,MONTHLY_HEIGHT_TOP,MONTHLY_HEIGHT_BOT $CLM_3_5_path $CLM5_path_cut_pft
 
+#ncks -A -v PCT_SAND,PCT_CLAY,MONTHLY_LAI,MONTHLY_SAI,MONTHLY_HEIGHT_TOP,MONTHLY_HEIGHT_BOT $CLM_3_5_path $CLM5_path_cut_pft
+ncks  -A -v PCT_SAND,PCT_CLAY,PCT_WETLAND,LANDFRAC_PFT,PFTDATA_MASK $CLM_3_5_path $CLM5_path_cut_pft
