7f0b30c88f74ef6e1518aa74cde3f7a64e8f16ce
Stefan Poll <s.poll@fz-juelich.de>
2023-07-20 13:53:34 +0200

    add year loop in ERA5 mkforcing

--- mkforcing/extract_ERA5_meteocloud.sh
+++ mkforcing/extract_ERA5_meteocloud.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# load env -> not all CDO are compiled with "-t ecmwf"
+# ml Stages/2022  NVHPC/22.9  ParaStationMPI/5.5.0-1 CDO/2.0.2
+
 for year in 2017
 do
 for month in 01 #02 03 04 05 06 07 08 09 10 11 12 
@@ -8,17 +12,17 @@ for my_date in $(seq -w 1 ${days_per_month})
 do
 for time in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 do
-cdo sellonlatbox,-48,74,20,69 /p/fastdata/slmet/slmet111/met_data/ecmwf/era5/grib/2017/${month}/2017${month}${my_date}${time}_ml.grb cut_domain_${month}${my_date}${time}.grb
+cdo sellonlatbox,-48,74,20,69 /p/fastdata/slmet/slmet111/met_data/ecmwf/era5/grib/${year}/${month}/${year}${month}${my_date}${time}_ml.grb cut_domain_${year}${month}${my_date}${time}.grb
 
-cdo sellevel,137 cut_domain_${month}${my_date}${time}.grb lower_level_${month}${my_date}${time}.grb
+cdo sellevel,137 cut_domain_${year}${month}${my_date}${time}.grb lower_level_${year}${month}${my_date}${time}.grb
 #cdo -t ecmwf -f nc4 copy lower_level_${month}${my_date}${time}.grb lower_level_${month}${my_date}${time}.nc
-cdo -t ecmwf selname,t,u,v,q lower_level_${month}${my_date}${time}.grb variables_lower_level_${month}${my_date}${time}.grb
+cdo -t ecmwf selname,t,u,v,q lower_level_${year}${month}${my_date}${time}.grb variables_lower_level_${year}${month}${my_date}${time}.grb
 
 
 done
 done
-cdo merge  variables_lower_level_*.grb meteocloud_${month}.grb
-rm variables_lower_level_*.grb cut_domain* lower_level_* 
-cdo -t ecmwf -f nc4 copy meteocloud_${month}.grb meteocloud_${month}.nc
+cdo merge  variables_lower_level_${year}*.grb meteocloud_${year}_${month}.grb
+rm variables_lower_level_${year}*.grb cut_domain_${year}* lower_level_${year}*
+cdo -t ecmwf -f nc4 copy meteocloud_${year}_${month}.grb meteocloud_${year}_${month}.nc
 done
 done
--- mkforcing/prepare_ERA5_v2.sh
+++ mkforcing/prepare_ERA5_v2.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
+for year in 2017
+do
 for month in 01 #02 03 04 05 06 07 08 09 10 11 12 
 do
 
-cdo expr,'WIND=sqrt(u^2+v^2)' meteocloud_${month}.nc temp.nc
+cdo expr,'WIND=sqrt(u^2+v^2)' meteocloud_${year}_${month}.nc ${year}_temp.nc
 
-cdo -f nc4c const,10,download${month}.nc.regrid.nc const.nc
+cdo -f nc4c const,10,download${year}${month}.nc.regrid.nc ${year}_const.nc
 
-ncpdq -U download${month}.nc.regrid.nc temp2.nc
+ncpdq -U download${year}${month}.nc.regrid.nc ${year}_temp2.nc
 
-cdo merge const.nc temp2.nc temp.nc temp3.nc
+cdo merge ${year}_const.nc ${year}_temp2.nc ${year}_temp.nc ${year}_temp3.nc
 
-ncrename -v sp,PSRF -v msdwswrf,FSDS -v msdwlwrf,FLDS -v mtpr,PRECTmms -v const,ZBOT -v t,TBOT -v q,SHUM temp3.nc
+ncrename -v sp,PSRF -v msdwswrf,FSDS -v msdwlwrf,FLDS -v mtpr,PRECTmms -v const,ZBOT -v t,TBOT -v q,SHUM ${year}_temp3.nc
 
-ncap2 -O -s 'where(FSDS<0.) FSDS=0' 2017_${month}.nc
+ncap2 -O -s 'where(FSDS<0.) FSDS=0' ${year}_${month}.nc
 
-ncatted -O -a units,ZBOT,m,c,"m" 2017_${month}.nc
+ncatted -O -a units,ZBOT,m,c,"m" ${year}_${month}.nc
 
-rm temp*
+rm ${year}_temp*nc ${year}_const.nc
 
+done
 done
