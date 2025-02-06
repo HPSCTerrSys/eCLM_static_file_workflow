b2b34bdf9516733a884ce9b95616a7be23db7004
Marco van Hulten <Marco.van.Hulten@uni-bonn.de>
2025-02-06 13:26:19 +0100

    Use shell variable instead of string

--- mod_domain.sh
+++ mod_domain.sh
@@ -3,18 +3,13 @@
 surface_file=surface.nc
 domain_file=domain.nc
 landmask_file=landmask.nc
+
 ncap2 -O -s 'where(LONGXY<0) LONGXY=LONGXY+360' ${surface_file} temp.nc
 mv temp.nc ${surface_file}
 
 ncap2 -O -s 'where(xc<0) xc=xc+360' ${domain_file} temp.nc
 ncap2 -O -s 'where(xv<0) xv=xv+360' temp.nc ${domain_file}
 
-ncrename -v LANDMASK,mask -v FRAC,frac landmask_file
-
+ncrename -v LANDMASK,mask -v FRAC,frac ${landmask_file}
 ncks -A -v mask ${landmask_file} ${domain_file}
-
 ncks -A -v frac ${landmask_file} ${domain_file}
-
-
-
-
